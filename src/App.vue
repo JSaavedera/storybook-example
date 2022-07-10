1c8fcbff118410e518a71c12178e9565a0d8fe6d
JSaavedra <jsaavedra@live.cl>
2022-07-10 19:35:25 -0400

    proyecto storybook

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+<div id="app">
+  <Button>Slot</Button>
+  <Button text="text"></Button>
+</div>
+</template>
+
+<script>
+
+import Button from "@/components/button.vue"
+
+export default {
+  name: "App",
+
+  components: { Button }
+}
+
+</script>
---
+++ src/components/button.vue
@@ -0,0 +1,25 @@
+<template>
+  <button
+    class="bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded"
+  >
+    <span v-if="isLoading"> Loading ... </span>
+    <span v-else-if="text">
+      {{ text }}
+    </span>
+    <slot v-else />
+  </button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+
+  props: {
+    text: { type: String, default: "" },
+    isLoading: { type: Boolean, default: false },
+    color: { type: String, default: "blue" },
+  },
+};
+</script>
+
+<style></style>
